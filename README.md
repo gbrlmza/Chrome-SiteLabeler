f922716de640f94dc7c90cde79cce0fde6131bb4
Gabriel Gonzalez <gbrlmza@gmail.com>
2015-11-10 20:48:40 -0300

    some improvements

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Chrome-SiteLabeler
 Chrome extension to add eye catching labels to sites depending on their url. Useful when working with different environments of the same site.
 
 Chrome Web Store URL: https://chrome.google.com/webstore/detail/site-labeler/mdkoanlgchgnkbfjcecafkadgneapali
+
+#### v0.2.0
+- Hide label on mouseover
+- Show label on mouse idle
+#### v0.1.0
+- Initial releas
--- content/content_script.js
+++ content/content_script.js
@@ -20,13 +20,54 @@ function convertHex(hex, opacity){
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.action == 'dolabel') {
 		var l = request.data.label;
-		deleteLabel();
 		var elem = document.createElement('div');
+		
+		deleteLabel();
+		
 		elem.id = labelId;
 		elem.className = 'chrome-extension-site-labeler-label chrome-extension-site-labeler-label-' + l.position;
-		elem.style.setProperty('background-color', convertHex(l.color, 90), 'important');
+		elem.style.setProperty('background-color', convertHex(l.color, 85), 'important');
 		elem.innerHTML = '<div>' + l.name + '</div>';
 		document.body.appendChild(elem);
+
+		// hide label on mouseover
+		elem.addEventListener("mouseover", function(event){
+			var lastmove, timer;
+			var elem = document.querySelector('#' + labelId);
+			var rect = elem.getBoundingClientRect(); // Get size and position before hide the label
+			
+			// Hide label
+			elem.style.setProperty('display', 'none', 'important');
+
+			// Clean-up and show label
+			var showLabel = function(){
+				document.removeEventListener("mousemove", handleMouseMove);
+				clearInterval(timer);
+				elem.style.setProperty('display', 'block', 'important');
+			};
+
+			// Show label if the mouse stay idle for a few seconds
+			timer = setInterval(function(){
+				var now = new Date().getTime();
+				if ( lastmove && (now - lastmove) > 4000 ) {
+					showLabel();
+				}
+			}, 1000);
+
+			var handleMouseMove = function(event){
+				var x = event.clientX;
+				var y = event.clientY;
+				
+				lastmove = new Date().getTime(); // save timestamp of last mouse movement
+
+				if ( x < rect.left || x > rect.right || y < rect.top || y > rect.bottom ) {
+					showLabel();
+				}
+			};
+
+			document.addEventListener("mousemove", handleMouseMove);
+			
+		});
 	}
 	return true;
 });
